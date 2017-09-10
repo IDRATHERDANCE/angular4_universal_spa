178162040b9b4d8bc1c6e540a44e5197bcb2c5f0
IDRATHER.DANCE <yes@idrather.dance>
2017-09-10 22:59:48 +0200

    procfile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,12 +35,12 @@ import { SubMenuPrettyPipe } from './shared/submenuPretty.pipe';
     NgReduxModule,
     RouterModule.forRoot([
       { path: '', loadChildren: './splash/splash.module#SplashModule'},
-      // { path: 'work', loadChildren: './work/work.module#WorkModule'},
-      // { path: 'news', loadChildren: './news/news.module#NewsModule'},
-      // { path: 'press', loadChildren: './press/press.module#PressModule'},
-      // { path: 'exhibitions', loadChildren: './exhibitions/exhibitions.module#ExhibitionsModule'},
-      // { path: 'about', loadChildren: './about/about.module#AboutModule'},
-      // { path: 'contact', loadChildren: './contact/contact.module#ContactModule'}
+      { path: 'work', loadChildren: './work/work.module#WorkModule'},
+      { path: 'news', loadChildren: './news/news.module#NewsModule'},
+      { path: 'press', loadChildren: './press/press.module#PressModule'},
+      { path: 'exhibitions', loadChildren: './exhibitions/exhibitions.module#ExhibitionsModule'},
+      { path: 'about', loadChildren: './about/about.module#AboutModule'},
+      { path: 'contact', loadChildren: './contact/contact.module#ContactModule'}
     ])
   ],
   declarations: [
