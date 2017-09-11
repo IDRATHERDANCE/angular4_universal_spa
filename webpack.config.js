7d090ed96707322ea630b480bc542810f4932881
IDRATHER.DANCE <yes@idrather.dance>
2017-09-11 11:18:21 +0200

    procfile

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,8 @@ module.exports = function (options, webpackOptions) {
 
   const configs = [];
   if (!options.aot) {
-    configs.push(clientConfig, serverConfig);
+    // configs.push(clientConfig, serverConfig);
+    configs.push(clientConfig);
 
   } else if (options.client) {
     configs.push(clientConfig);
