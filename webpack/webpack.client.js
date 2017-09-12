7c893c42c839b2fa5ea71f6e07e74825e56525d4
IDRATHER.DANCE <yes@idrather.dance>
2017-09-12 18:57:33 +0200

    gzip compression enabled

--- webpack/webpack.client.js
+++ webpack/webpack.client.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ScriptExtPlugin = require('script-ext-html-webpack-plugin');
 const webpack = require('webpack');
 const CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
-// const CompressionPlugin = require("compression-webpack-plugin");
 
 /**
  * This is a client config which should be merged on top of common config
@@ -31,14 +30,7 @@ module.exports = {
       }),
       new CommonsChunkPlugin({
         name: ['vendor', 'polyfills']
-      }),
-      // new CompressionPlugin({
-      //     asset: "[path].gz[query]",
-      //     algorithm: "gzip",
-      //     test: /\.(js|html)$/,
-      //     threshold: 10240,
-      //     minRatio: 0.8
-      // })
+      })
   ]
 };
 
